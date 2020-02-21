bb930fa55ecb1c4fa382b44f79bc764a83e82021
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-02-21 14:02:31 +0100

    Added example app

---
+++ Restler-Example/Restler-Example/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Restler-Example
+//
+//  Created by Bartłomiej Świerad on 21/02/2020.
+//  Copyright © 2020 Railwaymen. All rights reserved.
+//
+
+import SwiftUI
+import Restler
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
