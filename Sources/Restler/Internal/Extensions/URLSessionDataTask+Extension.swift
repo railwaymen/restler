686cfe8030a61d3c9b812f572fcefdcffd867702
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-03-04 11:49:42 +0100

    Added docs for public symbols in the Task class

--- Sources/Restler/Internal/Extensions/URLSessionDataTask+Extension.swift
+++ Sources/Restler/Internal/Extensions/URLSessionDataTask+Extension.swift
@@ -2,6 +2,7 @@ import Foundation
 
 protocol URLSessionDataTaskType: class {
     var taskIdentifier: Int { get }
+    var state: URLSessionTask.State { get }
     func cancel()
     func suspend()
     func resume()
