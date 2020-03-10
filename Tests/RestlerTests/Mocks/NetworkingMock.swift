6f0ec229fe277689ed24bc96068d63dba9913a34
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-03-10 16:10:50 +0100

    Added possibility of removing single header fields in a single request.

--- Tests/RestlerTests/Mocks/NetworkingMock.swift
+++ Tests/RestlerTests/Mocks/NetworkingMock.swift
@@ -4,35 +4,20 @@ import Foundation
 class NetworkingMock {
     
     // MARK: - NetworkingType
-    var headerReturnValue: Restler.Header = .init()
-    private(set) var headerSetParams: [HeaderSetParams] = []
-    struct HeaderSetParams {
-        let value: Restler.Header
-    }
-    
     var makeRequestReturnValue: Restler.Task?
     private(set) var makeRequestParams: [MakeRequestParams] = []
     struct MakeRequestParams {
         let url: URL
         let method: HTTPMethod
-        let customHeaderFields: Restler.Header
+        let header: Restler.Header
         let completion: DataCompletion
     }
 }
 
 // MARK: - NetworkingType
 extension NetworkingMock: NetworkingType {
-    var header: Restler.Header {
-        get {
-            self.headerReturnValue
-        }
-        set {
-            self.headerSetParams.append(HeaderSetParams(value: newValue))
-        }
-    }
-    
-    func makeRequest(url: URL, method: HTTPMethod, customHeaderFields: Restler.Header, completion: @escaping DataCompletion) -> Restler.Task? {
-        self.makeRequestParams.append(MakeRequestParams(url: url, method: method, customHeaderFields: customHeaderFields, completion: completion))
+    func makeRequest(url: URL, method: HTTPMethod, header: Restler.Header, completion: @escaping DataCompletion) -> Restler.Task? {
+        self.makeRequestParams.append(MakeRequestParams(url: url, method: method, header: header, completion: completion))
         return self.makeRequestReturnValue
     }
 }
