314fe740cdbfbc1c0044060c976cc28e665b755b
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-04-10 09:29:20 +0200

    Added job to build example app

--- Restler-Example/Restler-Example/AppDelegate.swift
+++ Restler-Example/Restler-Example/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
+
     }
 
 }
