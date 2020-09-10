36c2683cdfaf249a7f5595063220ab8653734f02
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-09-10 10:46:47 +0200

    Added support for RxSwift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
@@ -7,12 +7,23 @@ let package = Package(
         .library(
             name: "Restler",
             targets: ["Restler"]),
+        .library(
+            name: "RxRestler",
+            targets: ["RxRestler"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1")),
+    ],
     targets: [
         .target(
             name: "Restler",
             dependencies: []),
+        .target(
+            name: "RxRestler",
+            dependencies: [
+                "Restler",
+                .product(name: "RxSwift", package: "RxSwift"),
+            ]),
         .testTarget(
             name: "RestlerTests",
             dependencies: ["Restler"])
