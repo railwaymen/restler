4b424d66a7ed134cf47cc1ced71d23cbf76fef99
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-10-30 09:58:58 +0100

    Added caching to the bundler setup

--- Scripts/setup_bundler.sh
+++ Scripts/setup_bundler.sh
@@ -1,4 +1,5 @@
 bundle update --bundler # ensure bundler version is high enough for Gemfile.lock
-bundle install
+bundle config path vendor/bundle
+bundle install --jobs 8 --retry 3
 bundle --version
 bundle exec pod --version
